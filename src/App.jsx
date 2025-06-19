403bd312ee50995b982b9972f1d0f078b757f342
Derick Rono <168089581+codedbyte@users.noreply.github.com>
2025-06-19 23:22:23 +0300

    Add search and star SVG icons; implement Search component
    - Added search.svg and star.svg icons to the public directory.
    - Created a Search component in src/assets/components that includes an input field for searching movies in the gallery.
    - The Search component utilizes state to manage the search term input.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,86 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Search from './assets/components/Search.jsx'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3/';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  }
+}
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState('');
+  const [loadingState, setLoadingState] = useState(false);
+
+
+   const fetchMovies = async () => {
+    setLoadingState(true);
+    setErrorMessage('');
+
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error ('Failed to Fetch Movies');
+      }
+      
+
+      const data = await response.json();
+
+      console.log(data);
+
+      if (data.Response === "False"){
+        console.error(data.Error || 'Failed to Fetch Movie');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    }
+    catch (error) {
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    }
+    finally{
+      setLoadingState(false);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchMovies();
+  }, [])
+
+ 
   return (
-    <div>
-       <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+
+
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> you will enjoy without the Hassle. </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="error text-red-500">{errorMessage}</p>}
+        </section>
+      </div>
+    </main>
   )
 }
 
